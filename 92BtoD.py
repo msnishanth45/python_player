9747be0ffdffca4a08d1f6df04286e2e37677247
msnishanth45 <37323167+msnishanth45@users.noreply.github.com>
2019-08-10 22:28:41 +0530

    Create 92BtoD.py

---
+++ 92BtoD.py
@@ -0,0 +1,14 @@
+def binaryToDecimal(b): 
+    n = b; 
+    dec_value = 0; 
+    base = 1; 
+    temp = n; 
+    while(temp): 
+        last_digit = temp % 10; 
+        temp = int(temp / 10); 
+          
+        dec_value += last_digit * base; 
+        base = base * 2; 
+    return dec_value; 
+n = int(input()) 
+print(binaryToDecimal(n));
